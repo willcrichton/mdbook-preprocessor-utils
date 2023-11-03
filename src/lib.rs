8ccb196fedd15f3103a2d23828648297e29702fa
Will Crichton <wcrichto@cs.stanford.edu>
2023-11-03 09:24:59 -0700

    Add support for logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
-use std::{io, process};
+use std::{
+  env,
+  io::{self, Write},
+  process,
+};
 
+use chrono::Local;
 use clap::{Parser, Subcommand};
+use env_logger::Builder;
+use log::LevelFilter;
 use mdbook::{
   errors::Error,
   preprocess::{CmdPreprocessor, Preprocessor},
@@ -27,7 +34,37 @@ enum Command {
   Supports { renderer: String },
 }
 
+// This is copied verbatim from mdbook so the style is consistent.
+// https://github.com/rust-lang/mdBook/blob/94e0a44e152d8d7c62620e83e0632160977b1dd5/src/main.rs#L97-L121
+fn init_logger() {
+  let mut builder = Builder::new();
+
+  builder.format(|formatter, record| {
+    writeln!(
+      formatter,
+      "{} [{}] ({}): {}",
+      Local::now().format("%Y-%m-%d %H:%M:%S"),
+      record.level(),
+      record.target(),
+      record.args()
+    )
+  });
+
+  if let Ok(var) = env::var("RUST_LOG") {
+    builder.parse_filters(&var);
+  } else {
+    // if no RUST_LOG provided, default to logging at the Info level
+    builder.filter(None, LevelFilter::Info);
+    // Filter extraneous html5ever not-implemented messages
+    builder.filter(Some("html5ever"), LevelFilter::Error);
+  }
+
+  builder.init();
+}
+
 pub fn main<P: SimplePreprocessor>() {
+  init_logger();
+
   let args = Args::parse();
   let preprocessor = processor::SimplePreprocessorDriver::<P>::new();
 
