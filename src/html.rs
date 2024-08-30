30b1472311b6bbbace6c4650f75234668ff917da
Will Crichton <wcrichto@cs.stanford.edu>
2024-08-29 21:19:43 -0700

    Improve HTML API

--- src/html.rs
+++ src/html.rs
@@ -7,15 +7,21 @@ pub struct HtmlElementBuilder {
 }
 
 impl HtmlElementBuilder {
-  pub fn new(class: &str) -> Self {
-    let html = format!(r#"<div class="{class}" style="width: 1000px; height: 1000px""#);
+  pub fn new() -> Self {
+    let html = String::from("<div");
     HtmlElementBuilder { html }
+  }
+
+  pub fn attr(&mut self, key: &str, value: &str) -> Result<&mut Self> {
+    let value_escaped = html_escape::encode_double_quoted_attribute(&value);
+    write!(&mut self.html, " {key}=\"{value_escaped}\"")?;
+    Ok(self)
   }
 
   pub fn data(&mut self, key: &str, value: impl Serialize) -> Result<&mut Self> {
     let value_json = serde_json::to_string(&value)?;
     let value_escaped = html_escape::encode_double_quoted_attribute(&value_json);
-    write!(&mut self.html, " data-{key}=\"{value_escaped}\" ",)?;
+    write!(&mut self.html, " data-{key}=\"{value_escaped}\"",)?;
     Ok(self)
   }
 
